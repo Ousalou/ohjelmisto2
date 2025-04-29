f165460bb1449c76474072fb5d8eeaa1d3896668
Ousalou <osalonen@icloud.com>
2025-04-29 12:26:31 +0300

    m10

--- m10/m10_04.py
+++ m10/m10_04.py
@@ -14,9 +14,10 @@
 #kilpailu_ohi, joka palauttaa True, jos jokin autoista on maalissa eli se on ajanut vähintään kilpailun kokonaiskilometrimäärän. Muussa tapauksessa palautetaan False
 
 
-# Kirjoita pääohjelma, joka luo 8000 kilometrin kilpailun nimeltä "Suuri romuralli".
+# Kirjoita pääohjelma, joka luo 8000 kilometrin kilpailun liniment "Suuri romuralli".
 # Luotavalle kilpailulle annetaan kymmenen auton lista samaan tapaan kuin aiemmassa tehtävässä.
 # Pääohjelma simuloi kilpailun etenemistä kutsumalla toistorakenteessa tunti_kuluu-metodia, jonka jälkeen aina tarkistetaan kilpailu_ohi-metodin avulla, onko kilpailu ohi.
+
 # Ajantasainen tilanne tulostetaan tulosta tilanne-metodin avulla kymmenen tunnin välein sekä kertaalleen sen jälkeen, kun kilpailu on päättynyt.
 
 
@@ -26,14 +27,13 @@ tunti_laskuri = 0
 class Kilpailu:
     def __init__(kisa, nimi, pituus):
         kisa.autot = []
-        for i in range(1,10):
+        for i in range(0,10):
             auto = Auto()
             kisa.autot.append(auto)
         kisa.nimi = nimi
-        kisa.pituus = pituus
+        kisa.pituus = int(pituus)
 
     def tunti_kuluu(kisa):
-        Kilpailu.tunti_laskuri = Kilpailu.tunti_laskuri + 1
         for i in range (0, len(kisa.autot)):
             print(f"\nAUTO {kisa.autot[i].rekisteritunnus}\n-------------")
             delta_nopeus_rand = random.randint(-10, 15)
@@ -43,17 +43,21 @@ class Kilpailu:
             print(f"Tämänhetkinen nopeus: {kisa.autot[i].current_nopeus}km/h\n")
 
     def tulosta_tilanne(kisa):
+        tilanne_lista = sorted(kisa.autot, key=lambda x: x.current_matka, reverse=True)
         print(f"{'-' * 80}")
-        print(f"{'Rekisteritunnus':<20} | {'Nopeus nyt':<20} | {'Kuljettu matka':<21} |)
+        print(f"{'Rekisteritunnus':<20} | {'Nopeus nyt':<20} | {'Kuljettu matka':<21} |")
         print(f"{'-' * 80}")
-        for i in range (0, len(kisa.autot)):
+        for i in tilanne_lista:
             print(f"{'-' * 80}")
-            print(f"{kisa.autot[i].rekisteritunnus:<20} | {kisa.autot[i].current_nopeus:<20} | {kisa.autot[i].current_matka:<21} |)
+            print(f"{i.rekisteritunnus:<20} | {i.current_nopeus:<20} | {i.current_matka:<21} |")
             print(f"{'-' * 80}")
         print(f"{'-' * 80}")
 
     def kilpailu_ohi(kisa):
-
+        for i in range(len(kisa.autot)):
+            if kisa.autot[i].current_matka >= 8000:
+                return True
+        return False
 
 class Auto:
     def __init__(auto):
@@ -77,3 +81,18 @@ class Auto:
             auto.current_nopeus = auto.current_nopeus + delta_nopeus_rand
         auto.current_matka = auto.current_matka + auto.current_nopeus
 
+ralli = Kilpailu("Suuri romuralli", 8000)
+
+kisa_ohi = False
+h_counter = 10
+
+while not kisa_ohi:
+    Kilpailu.tunti_kuluu(ralli)
+    kisa_ohi = Kilpailu.kilpailu_ohi(ralli)
+    h_counter = h_counter - 1
+    if h_counter == 1:
+        Kilpailu.tulosta_tilanne(ralli)
+        h_counter = 10
+
+print(f"\nKilpailu ohi!\n")
+Kilpailu.tulosta_tilanne(ralli)
